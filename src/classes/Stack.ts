8273df32b2ee511877e7d939c0aac5e48521b13e
Swarup Lamichhane <lswarup1234@gmail.com>
2024-07-06 09:20:17 +0545

    Added class definitions for Stack and Queue

---
+++ src/classes/Stack.ts
@@ -0,0 +1,32 @@
+import { IStack } from "../interfaces/IStack";
+
+class Stack<T> implements IStack<T> {
+  private storage: T[] = [];
+
+  constructor(private capacity: number = Infinity) {}
+
+  push(item: T): void {
+    if (this.size() === this.capacity) {
+      throw Error("Stack has reached max capacity, you cannot add more items");
+    }
+    this.storage.push(item);
+  }
+
+  pop(): T | undefined {
+    return this.storage.pop();
+  }
+
+  peek(): T | undefined {
+    return this.storage[this.size() - 1];
+  }
+
+  size(): number {
+    return this.storage.length;
+  }
+
+  isEmpty(): boolean {
+    return this.storage.length === 0;
+  };
+}
+
+export default Stack;
