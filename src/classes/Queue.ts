b1f0c868330903d5d99daf89c78457dc2e6b6e4b
Swarup Lamichhane <lswarup1234@gmail.com>
2024-07-07 09:17:51 +0545

    refactor: Update Queue class to support generic types

--- src/classes/Queue.ts
+++ src/classes/Queue.ts
@@ -1,9 +1,14 @@
 import { IQueue } from "../interfaces/IQueue";
+import { Cell } from "../types";
 
-class Queue<T> implements IQueue<T> {
+class Queue<T extends Cell> implements IQueue<T> {
   private storage: T[] = [];
 
   constructor(private capacity: number = Infinity) {}
+
+  getStorage(): T[] {
+    return this.storage;
+  }
 
   enqueue(item: T): void {
     if (this.size() === this.capacity) {
@@ -16,6 +21,10 @@ class Queue<T> implements IQueue<T> {
   }
   size(): number {
     return this.storage.length;
+  }
+
+  has(item: T): boolean {
+    return this.storage.some((i) => i[0] === item[0] && i[1] === item[1]);
   }
 
   isEmpty(): boolean {
