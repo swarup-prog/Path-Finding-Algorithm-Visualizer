fe8b4a55785655bbfb68cff2c2f25e424b12c5e6
Swarup Lamichhane <lswarup1234@gmail.com>
2024-07-07 09:52:55 +0545

    chore: refactore code

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -12,7 +12,6 @@ export const breadthFirstSearch = (
   const visited: Cell[] = [];
 
   queue.enqueue(start);
-  let stopper = 0;
 
   while (!queue.isEmpty()) {
     const current = queue.dequeue() as Cell;
@@ -22,24 +21,14 @@ export const breadthFirstSearch = (
     if (current === end) return visited;
 
     const neighbours = getNeighbours(maze, current);
-    console.log(
-      `Visited: ${visited}\nCurrent: ${current}\nNeighbours: ${neighbours}`
-    );
 
     for (let neighbour of neighbours) {
       const neighbourVisited = isVisited(neighbour, visited);
-      console.log("Current Neighbour:", neighbour);
-      console.log("Neighbour Visited:", neighbourVisited);
       if (!neighbourVisited && !queue.has(neighbour)) {
         queue.enqueue(neighbour);
-        // console.log("Queue:", queue.getStorage());
-      } else continue;
-    } // if (stopper > 5) break;
-    // stopper++;
+      }
+    }
   }
-
-  console.log("Queue: ", queue.getStorage());
-  console.log("Visited: ", JSON.stringify(visited.length));
 
   return visited;
 };
