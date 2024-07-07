44e16bb2d053250a72db6097b447263583af068c
Swarup Lamichhane <lswarup1234@gmail.com>
2024-07-07 09:26:41 +0545

    feat: Add color prop to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,14 @@
 interface IButtonProps {
   title: string;
+  color?: string;
   onClick: () => void;
 }
 
-const Button = ({ title, onClick }: IButtonProps) => {
+const Button = ({ title, color = "orange", onClick }: IButtonProps) => {
   return (
     <button
-      className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
+      // style={{ backgroundColor: color ?? "" }}
+      className={`bg-${color}-500 hover:bg-${color}-700 text-white font-bold py-2 px-4 rounded`}
       onClick={onClick}
     >
       {title}
