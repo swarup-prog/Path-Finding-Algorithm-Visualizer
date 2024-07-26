3a0724472eef20ed377dd04c166d8d28481b6679
Swarup Lamichhane <swarup.lamichhane@innovatetech.co>
2024-07-26 09:37:20 +0545

    refactor: Add algorithmStateSlice to manage algorithm state in Redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import mazeStateReducer from '../features/mazeState/mazeStateSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import mazeStateReducer from "../features/mazeState/mazeStateSlice";
+import algorithmStateReducer from "../features/algorithmState/algorithmStateSlice";
 
 export const store = configureStore({
   reducer: {
     mazeState: mazeStateReducer,
+    algorithmState: algorithmStateReducer,
   },
-})
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
